7f3a56288608af3a3693a3fdfacfd72ee0f20e66
Brad Jones <BJ2044@gmail.com>
2022-10-13 10:38:13 -0400

    guessing is fun

--- guess.js
+++ guess.js
@@ -6,3 +6,32 @@ if it's the right number say "congratulations" and exit
 
 */
 import readline from "readline-sync"
+
+let userGuessedRight = false
+
+//while(true) { // me playing
+    // guess a number fom 1-10
+let NumberChosen = Math.ceil(Math.random() *10)
+//console.log(NumberChosen)
+
+while(!userGuessedRight) {
+let guess = readline.question("what is your guess?")
+if (guess == NumberChosen)
+    {
+        console.log("congratulations")
+        userGuessedRight = true
+        }
+if (guess > NumberChosen)
+    {
+        console.log("too high try again buddy")
+        }
+if (guess < NumberChosen)
+    {
+        console.log("too low buddy try again")
+        }
+//console.log(guess)
+}
+//}
+//while(!userGuessedRight) {
+//    console.log("what is your guess?")
+//}
